e0d10bbac1ebf16dcd8e652c9a4674dff6587c83
AlenaBelenets <alenaharenkova@gmail.com>
2022-07-20 22:05:36 +1000

    Initial Commit

---
+++ PesonalList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PesonalList
+//
+//  Created by Alena Belenets on 20.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
